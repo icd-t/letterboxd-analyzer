e4ebbf8a5ffbbf178a84719a40ae55af84dbae6b
icd-t <139998388+icd-t@users.noreply.github.com>
2026-09-15 15:07:54 -0400

    refactored by-year section to use a single array of tuples

--- analyze.cpp
+++ analyze.cpp
@@ -31,52 +31,48 @@ int main() {
 
   // Count volume by year
   cout << "Counting years..." << endl;
-  // This array will store data for all years
-  // It is indexed such that 0 is 1826 and 199 is 2025
-  // Column 0 is just a count of movies for that year
-  // Column 1 is a count of non-documentaries for that year
-  int years[200][2]{};
-  // Stores average rating per year
-  // First row is not weighted
-  // Second row is weighted by popularity score
-  double yearsRatings[200][2]{};
-  // Popularity scores per year
-  double yearsPopularity[200]{};
+  // This array of tuples holds all the data that will be outputted to the csv
+  // file. Variables from left to right: count of films in that year, count of
+  // non-documentaries in that year, average rating, average rating (weighted by
+  // popularity), total popularity score per year
+  array<tuple<int, int, double, double, double>, 200> byYear{};
   // Loop over all rows
   for (int i = 1; i < df.n_rows(); i++) {
     // Increment count for whatever year the current row is
-    years[(df[i]["year"].get<int>() - 1826)][0] += 1;
+    get<0>(byYear[df[i]["year"].get<int>() - 1826]) += 1;
     // Increment count for excluding-docs years if neither genre of current row
     // is documentary
     if (!checkGenre(df, i, "Documentary")) {
-      years[df[i]["year"].get<int>() - 1826][1] += 1;
+      get<1>(byYear[df[i]["year"].get<int>() - 1826]) += 1;
     }
     // Increment average rating
-    yearsRatings[(df[i]["year"].get<int>() - 1826)][0] +=
+    get<2>(byYear[(df[i]["year"].get<int>() - 1826)]) +=
         df[i]["rating"].get<double>();
     // Increment average rating popularity weighted
-    yearsRatings[(df[i]["year"].get<int>() - 1826)][1] +=
+    get<3>(byYear[(df[i]["year"].get<int>() - 1826)]) +=
         (df[i]["rating"].get<double>() * df[i]["popularity"].get<double>());
     // Increment populatity
-    yearsPopularity[(df[i]["year"].get<int>() - 1826)] +=
+    get<4>(byYear[(df[i]["year"].get<int>() - 1826)]) +=
         df[i]["popularity"].get<double>();
   }
   cout << "Outputting years..." << endl;
   f01 << "year,count,count-exdocs,popularity,rating,rating-pop-weighted\n";
-  for (int i = 0; i < size(years); i++) {
+  for (int i = 0; i < size(byYear); i++) {
     // Average the ratings column
-    if (yearsRatings[i][0] != 0) {
-      yearsRatings[i][0] = (yearsRatings[i][0] / years[i][0]);
-      yearsRatings[i][1] = (yearsRatings[i][1] / yearsPopularity[i]);
+
+    if (get<2>(byYear[i]) != 0) {
+      get<2>(byYear[i]) = get<2>(byYear[i]) / get<0>(byYear[i]);
+      get<3>(byYear[i]) = get<3>(byYear[i]) / get<4>(byYear[i]);
     } else {
-      yearsRatings[i][0] = 0;
-      yearsRatings[i][1] = 0;
+      get<2>(byYear[i]) = 0;
+      get<3>(byYear[i]) = 0;
     }
     // Output
     currentRow =
-        (to_string((i + 1826)) + "," + to_string(years[i][0]) + "," +
-         to_string(years[i][1]) + "," + to_string(yearsPopularity[i]) + "," +
-         to_string(yearsRatings[i][0]) + "," + to_string(yearsRatings[i][1]));
+        (to_string((i + 1826)) + "," + to_string(get<0>(byYear[i])) + "," +
+         to_string(get<1>(byYear[i])) + "," + to_string(get<2>(byYear[i])) +
+         "," + to_string(get<3>(byYear[i])) + "," +
+         to_string(get<4>(byYear[i])));
     // cout << currentRow << endl;
     f01 << currentRow << "\n";
   }
